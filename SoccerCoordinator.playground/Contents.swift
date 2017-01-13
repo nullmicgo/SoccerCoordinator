1e8ca8dad925b61d9bec9d7775e383d8d1348d14
michael <michael@stephaniechois-mac-mini.local>
2017-01-14 00:09:07 +0800

    ** Part 1: ** We have provided information for the 18 players in the Player Info spreadsheet. Please choose an appropriate data type to store the information for each player. In addition, create an empty collection variable to hold all the players’ data. Once you have decided on what tools to use, manually enter the player data so it can be used in Part 2.

---
+++ SoccerCoordinator.playground/Contents.swift
@@ -0,0 +1,137 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Constants for dictionary keys
+let name: String = "name"
+let height: String = "height (inches)"
+let soccerExperience: String = "soccer experience"
+let guardianNames: String = "guardian Name(s)"
+
+let joeSmith: Dictionary<String, Any> = [
+    name: "Joe Smith",
+    height: 42,
+    soccerExperience: true,
+    guardianNames: "Jim and Jan Smith"
+]
+
+let jillTanner: Dictionary<String, Any> = [
+    name: "Jill Tanner",
+    height: 36,
+    soccerExperience: true,
+    guardianNames: "Clara Tanner"
+]
+
+let billBon: Dictionary<String, Any> = [
+    name: "Bill Bon",
+    height: 43,
+    soccerExperience: true,
+    guardianNames: "Sara and Jenny Bon"
+]
+
+let evaGordon: Dictionary<String, Any> = [
+    name: "Eva Gordon",
+    height: 45,
+    soccerExperience: false,
+    guardianNames: "Wendy and Mike Gordon"
+]
+
+let mattGill: Dictionary<String, Any> = [
+    name: "Matt Gill",
+    height: 40,
+    soccerExperience: false,
+    guardianNames: "Charles and Sylvia Gill"
+]
+
+let kimmyStein: Dictionary<String, Any> = [
+    name: "Kimmy Stein",
+    height: 41,
+    soccerExperience: false,
+    guardianNames: "Bill and Hillary Stein"
+]
+
+let sammyAdams: Dictionary<String, Any> = [
+    name: "Sammy Adams",
+    height: 45,
+    soccerExperience: false,
+    guardianNames: "Jeff Adams"
+]
+
+let karlSaygan: Dictionary<String, Any> = [
+    name: "Karl Saygan",
+    height: 42,
+    soccerExperience: true,
+    guardianNames: "Heather Bledsoe"
+]
+
+let suzaneGreenbergi: Dictionary<String, Any> = [
+    name: "Suzane Greenberg",
+    height: 44,
+    soccerExperience: true,
+    guardianNames: "Henrietta Dumas"
+]
+
+let salDali: Dictionary<String, Any> = [
+    name: "Sal Dali",
+    height: 41,
+    soccerExperience: false,
+    guardianNames: "Gala Dali"
+]
+
+let joeKavalier: Dictionary<String, Any> = [
+    name: "Joe Kavalier",
+    height: 39,
+    soccerExperience: false,
+    guardianNames: "Sam and Elaine Kavalier"
+]
+
+let benFinkelstein: Dictionary<String, Any> = [
+    name: "Ben Finkelstein",
+    height: 44,
+    soccerExperience: false,
+    guardianNames: "Aaron and Jill Finkelstein"
+]
+
+let diegoSoto: Dictionary<String, Any> = [
+    name: "Diego Soto",
+    height: 41,
+    soccerExperience: true,
+    guardianNames: "Robin and Sarika Soto"
+]
+
+let chloeAlaska: Dictionary<String, Any> = [
+    name: "Chloe Alaska",
+    height: 47,
+    soccerExperience: false,
+    guardianNames: "David and Jamie Alaska"
+]
+
+let arnoldWillis: Dictionary<String, Any> = [
+    name: "Arnold Willis",
+    height: 43,
+    soccerExperience: false,
+    guardianNames: "Claire Willis"
+]
+
+let phillipHelm: Dictionary<String, Any> = [
+    name: "Phillip Helm",
+    height: 44,
+    soccerExperience: true,
+    guardianNames: "Thomas Helm and Eva Jones"
+]
+
+let lesClay: Dictionary<String, Any> = [
+    name: "Les Clay",
+    height: 42,
+    soccerExperience: true,
+    guardianNames: "Wynonna Brown"
+]
+
+let herschelKrustofski: Dictionary<String, Any> = [
+    name: "Herschel Krustofski",
+    height: 45,
+    soccerExperience: true,
+    guardianNames: "Hyman and Rachel Krustofski"
+]
